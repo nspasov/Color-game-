daa21ea46573910d2b4a7da6996a51c19d2f2464
Nikolay Spasov <nspasov22@gmail.com>
2020-08-03 09:33:38 +0300

    First Commit

---
+++ colorGame.js
@@ -0,0 +1,127 @@
+var squareCount = 6;
+var colors = [];
+var pickedColor ;
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#resetButton");
+var modeButtons = document.querySelectorAll(".mode");
+var h1 = document.querySelector("h1");
+
+
+init();
+
+function init(){
+
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons(){
+	for(var i = 0; i<modeButtons.length; i++){
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.toggle("selected");
+			modeButtons[1].classList.toggle("selected");
+
+			if(this.textContent == "Easy"){
+				squareCount = 3;
+			}else{
+				squareCount = 6;
+			}
+			reset();
+		});
+	}
+}
+
+function setupSquares(){
+	for(var i=0; i < squares.length; i++){
+	//add initial colors to squares
+		squares[i].style.backgroundColor = colors[i];
+
+		//click events
+		squares[i].addEventListener("click", function(){
+			// get color of clicked square
+			var clickedColor = this.style.backgroundColor;
+			//compare color to picked color
+			if(clickedColor == pickedColor){
+				messageDisplay.textContent = "Correct!";
+				changeColors(clickedColor);
+				h1.style.backgroundColor = pickedColor;
+				resetButton.textContent = "Play again?";
+			}else{
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try again!";
+			}
+		});
+	}
+}
+
+
+
+
+function reset(){
+	// go back to original h1 background color
+	h1.style.backgroundColor = "steelblue";
+	// generate new colors
+	colors = generateRandomColors(squareCount);
+	// pick new random color
+	pickedColor = pickColor();
+	// change color display to match picked color
+	colorDisplay.textContent = pickedColor;
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		}else{
+			squares[i].style.display = "none";
+		}
+		
+	}
+	resetButton.textContent = "New colors";
+	messageDisplay.textContent = "";
+}
+
+
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+
+
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	 /*  Math.Floor(Math.random()*6 + 1); 
+	     This is ACTUALLY the way to get a random number between 1 and 6.. man Javascript is a bitch  */
+	var random = Math.floor(Math.random()*colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+
+	var arr = [];
+
+	for(var i = 0; i < num; i++){
+		arr.push(randomColor());
+	}
+
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);  // get random number between 0 & 255
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+
+	var color = "rgb(" + r +", " + g +", " + b +")";
+
+	return color;
+
+}
